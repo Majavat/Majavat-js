1bbf0ce116bde084f3cbd16eae6a9bbf73e877fb
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-08-30 22:29:52 +0200

    Close #11 - pass indexJS template test

--- src/tests/indexJS.test.js
+++ src/tests/indexJS.test.js
@@ -1,5 +1,11 @@
+const indexJS = require('../templates/indexJS');
+
+const mockedTemplate = `
+const app = document.querySelector("#app");
+`;
+
 describe('indexJS template', () => {
     it('should return the required js template', () => {
-        expect(true).toBeFalsy();
+        expect(indexJS()).toBe(mockedTemplate);
     });
 });
