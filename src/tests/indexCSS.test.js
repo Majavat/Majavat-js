6b765fd128352f6e499c7917855dd7d20065efa4
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-08-30 22:10:50 +0200

    Close #10 - pass indexCSS template test

--- src/tests/indexCSS.test.js
+++ src/tests/indexCSS.test.js
@@ -1,5 +1,14 @@
+const indexCSS = require('../templates/indexCSS');
+
+const mockedTemplate = `
+    body {
+        padding: 0;
+        margin: 0;
+    }
+`;
+
 describe('indexCSS template', () => {
     it('should return the required css template', () => {
-        expect(true).toBeFalsy();
+        expect(indexCSS()).toBe(mockedTemplate);
     });
 });
