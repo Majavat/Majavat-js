0d0660a086e1d469fdc73f5f2306c9566045919d
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-02 23:30:56 +0200

    Close #33 - create createFile helper

---
+++ src/helpers/createFile.js
@@ -0,0 +1,31 @@
+// importing native modules
+const fs = require('fs');
+const path = require('path');
+
+// importing helper modules
+const cli = require('./cli');
+
+// create a file based on its input & template
+const createFile = (dir, file, template) => {
+
+    // stop creating the file if the input is undefined
+    if ( typeof file === "undefined" || typeof template === "undefined" ) {
+        console.log('\ninvalid file name');
+    } else {
+
+        // check if directory exists before attempting to create it
+        if ( !fs.existsSync(path.join(dir, file)) ) {
+            console.log(`\ncreating ${file} file`);
+            // concatenate directory and file path then create the file based on input
+            fs.writeFileSync(path.join(dir, file), template, 'utf-8');
+            console.log('done!');
+        } else {
+            console.log('\nfile exists!');
+            cli.close();
+        }
+
+    }
+
+};
+
+module.exports = createFile;
