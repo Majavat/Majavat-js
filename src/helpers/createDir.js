b44e703410e70c2d38dac4a6b31b82f452117383
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-02 23:31:43 +0200

    Close #28 - create createDir helper

---
+++ src/helpers/createDir.js
@@ -0,0 +1,29 @@
+// importing native modules
+const fs = require('fs');
+
+// importing helper modules
+const cli = require('./cli');
+
+// create a directory based on its input
+const createDir = (dir) => {
+
+    // stop creating the directory if the input is undefined
+    if ( typeof dir === "undefined" ) {
+        console.log('\ninvalid directory name');
+    } else {
+
+        // check if directory exists before attempting to create it
+        if ( !fs.existsSync(dir) ) {
+            console.log(`\ncreating ${dir} directory`);
+            fs.mkdirSync(dir);
+            console.log('done!');
+        } else {
+            console.log('\ndirectory exists!');
+            cli.close();
+        }
+
+    }
+    
+};
+
+module.exports = createDir;
