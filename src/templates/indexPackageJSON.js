5829fb9a2be9906af0867d4dc32349b62753a516
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-03 23:26:06 +0200

    Close #35 - create indexPackageJSON template

---
+++ src/templates/indexPackageJSON.js
@@ -0,0 +1,26 @@
+const indexPackageJSON = (project) => `{
+    "name": "${project.name}",
+    "version": "${project.version.length > 0 ? project.version : ""}",
+    "description": "${project.description.length > 0 ? project.description : ""}",
+    "main": "index.js",
+    "scripts": {
+        "test": "jest --verbose",
+        "coverage": "jest --coverage",
+    },
+    "repository": {
+        "type": "git",
+        "url": "${project.git.length > 0 ? project.git : ""}"
+    },
+    "keywords": [
+        ${project.keywords.length > 0 ? project.keywords :  ""}
+    ],
+    "author": "${project.author.length > 0 ? project.author : ""}",
+    "license": "${project.license.length > 0 ? project.license : "MIT"}",
+    "bugs": {
+        "url": "${project.url.length > 0 ? project.url`/issues` : ""}" 
+    },
+    "homepage": "${project.url.length > 0 ? project.url`#readme` : ""}"
+}
+`;
+
+module.exports = indexPackageJSON;
