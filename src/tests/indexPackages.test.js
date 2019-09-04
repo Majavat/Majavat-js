d077d4e483225c7bc7e94f5834c8658dd3c44121
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-04 23:36:51 +0200

    Close #43 - pass unit tests for indexGitignore template

--- src/tests/indexPackages.test.js
+++ src/tests/indexPackages.test.js
@@ -1,5 +1,9 @@
+const indexPackages = require('../templates/indexPackages');
+
+const mockedTemplate = 'npm i -D jest';
+
 describe('indexPackages template', () => {
     it('should return the required indexPackages template', () => {
-        expect(true).toBe(false);
+        expect(indexPackages()).toBe(mockedTemplate);
     });
 });
