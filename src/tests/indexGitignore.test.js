90838f14468b1f3f1c24a21e748f3b665c02dea0
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-05 00:57:38 +0200

    Close #51 - pass unit tests for indexGitignore template

--- src/tests/indexGitignore.test.js
+++ src/tests/indexGitignore.test.js
@@ -1,5 +1,11 @@
+const indexGitignore = require('../templates/indexGitignore');
+
+const mockedTemplate = `node_modules
+coverage
+`;
+
 describe('indexGitignore template', () => {
     it('should return the required indexGitignore template', () => {
-        expect(true).toBe(false);
+        expect(indexGitignore()).toBe(mockedTemplate);
     });
 });
