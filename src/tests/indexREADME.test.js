6274c4d7a0091f2f075814ad148f97bfcb68a643
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-04 23:49:45 +0200

    Close #41 - pass unit tests for indexREADME template

--- src/tests/indexREADME.test.js
+++ src/tests/indexREADME.test.js
@@ -1,5 +1,12 @@
+const indexREADME = require('../templates/indexREADME');
+
+const mockedTemplate = `# Majavat Project
+
+majavat project description
+`;
+
 describe('indexREADME template', () => {
     it('should return the required README template', () => {
-        expect(true).toBe(false);
+        expect(indexREADME('Majavat', 'majavat project description')).toBe(mockedTemplate);
     });
 });
