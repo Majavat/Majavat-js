cd64366f86d25a69a207ee13db125b14fb228161
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-09-05 00:33:24 +0200

    Close #42 - pass unit tests for indexPackageJSON template

--- src/tests/indexPackageJSON.test.js
+++ src/tests/indexPackageJSON.test.js
@@ -1,5 +1,43 @@
+const indexPackageJSON = require('../templates/indexPackageJSON');
+
+const mockedData = {
+    name: "Majavat",
+    version: "2.0",
+    description: "Majavat Project",
+    git: "https://github.com/Majavat/Majavat-js.git",
+    keywords: ["majavat", "project-generator", "boilerplate", "lighweight"],
+    author: "Majavat",
+    license: "ISC",
+    url: "https://github.com/Majavat/Majavat-js"
+}
+
+const mockedTemplate = `{
+    "name": "Majavat",
+    "version": "2.0",
+    "description": "Majavat Project",
+    "main": "index.js",
+    "scripts": {
+        "test": "jest --verbose",
+        "coverage": "jest --coverage",
+    },
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/Majavat/Majavat-js.git"
+    },
+    "keywords": [
+        "majavat", "project-generator", "boilerplate", "lighweight"
+    ],
+    "author": "Majavat",
+    "license": "ISC",
+    "bugs": {
+        "url": "https://github.com/Majavat/Majavat-js/issues" 
+    },
+    "homepage": "https://github.com/Majavat/Majavat-js#readme"
+}
+`;
+
 describe('indexPackageJSON template', () => {
     it('should return the required indexPackageJSON template', () => {
-        expect(true).toBe(false);
+        expect(indexPackageJSON(mockedData)).toBe(mockedTemplate);
     });
 });
